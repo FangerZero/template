93690fa2c67a7915fbaf6cac5df4bca487a5eae5
FangerZero <FangerZero@hotmail.com>
2020-03-14 01:21:07 -0500

    Added placeholder pages

---
+++ src/Pages/Page1/Page1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Page1 extends Component {
+    render() {
+        return (
+            <div>
+                This is Page1 for now
+            </div>
+        )
+    }
+}
+
+export default Page1
